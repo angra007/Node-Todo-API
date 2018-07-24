ddd2fdf0e0b2e85ea5d460640ce81dc693a06b4a
Ankit <angraankit@gmail.com>
2018-07-24 14:25:16 -0400

    Auth API

--- server/server.js
+++ server/server.js
@@ -55,17 +55,20 @@ app.get ('/todos/:id', (req, res) => {
 });
 
 app.post ('/user', (req, res) => {
-    console.log (res.body);
+    
 
-    var body = _.pick (req.body, ['email','password']);
-    var user = new User (body);
+    var user = new User ({
+        email : req.body.email,
+        password : req.body.password
+    });
 
-    user.save ().then ( (response) => {
-        console.log ('Sending response');
-        res.send (response);
-    }, (error) => {
-        console.log ('Error response');
-        res.staus (400).send (error);
+    user.save ().then ( () => {
+        return user.generateAuthToken ();
+    }).then ((token) => {
+        console.log (user);
+        res.header('x-auth', token).send (user);
+    }).catch ((error) => {
+        res.status (400).send (error);
     });
 
 })
